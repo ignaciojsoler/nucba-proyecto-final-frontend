9e0a7e158801590d18dbe061baba7c9610522fe3
Ignacio <ignaciojsoler@gmail.com>
2023-08-08 12:40:06 -0300

    Feature: banner and category cards

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface CategoryCardProps {
   data: {
     name: string;
@@ -11,9 +9,9 @@ interface CategoryCardProps {
 const CategoryCard = (props: CategoryCardProps) => {
   const {data} = props;
   return (
-    <div className='rounded-lg bg-slate-800 bg-opacity-40 backdrop-blur-lg overflow-hidden h-24 p-3 flex w-full md:h-26 lg:h-32'>
-      <div className='absolute -z-10 w-full h-full top-0 left-0 transition hover:scale-105'>
-        <div className='h-full w-full bg-slate-900 absolute top-0 left-0 opacity-70'></div>
+    <div className='group rounded-lg bg-slate-800 backdrop-blur-lg overflow-hidden h-24 p-3 flex w-full cursor-pointer md:h-26 lg:h-32'>
+      <div className='absolute -z-10 w-full h-full top-0 left-0 transition group-hover:scale-105'>
+        <div className='h-full w-full bg-slate-900 absolute top-0 left-0 opacity-70 transition-opacity duration-150 group-hover:opacity-50'></div>
         <img src={data.image} alt="category image" className='object-cover h-full w-full'/>
       </div>
       <h5 className='font-bold '>{data.name}</h5>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,43 @@
 import Input from "../components/Input";
 import { useState } from "react";
-import data from '../data/categories.json';
+import data from "../data/categories.json";
 import CategoryCard from "../components/CategoryCard";
+import banner from '../assets/img/banner.jpg';
 
 const Home = () => {
   const [searchInput, setSearchInput] = useState<string>("");
   return (
-    <div className="min-h-screen max-w-7xl m-auto px-6">
-      <div className="flex flex-col items-center pt-28 space-y-6">
-        <h3 className=" text-3xl font-semibold">¿Qué estás buscando?</h3>
-        <Input onChangeText={setSearchInput} value={searchInput} className="w-80 md:w-96" placeholder="Buscar..."/>
-        <div className="grid grid-cols-2 gap-3 w-full p-3 md:p-0 md:grid-cols-4 ">
+    <main className="min-h-screen max-w-7xl m-auto px-6">
+      <section className="flex flex-col items-center pt-28 space-y-6">
+        <div className=" rounded-xl overflow-hidden">
+          <img src={banner} alt="banner" />
+        </div>
+        {/* <h3 className=" text-3xl font-semibold">¿Qué estás buscando?</h3> */}
+        {/* <Input
+          onChangeText={setSearchInput}
+          value={searchInput}
+          className="w-80 md:w-96"
+          placeholder="Buscar..."
+        /> */}
+        <div className="w-full space-y-4">
+          <h4 className=" text-2xl font-semibold self-start">Categorías</h4>
+          <div className="grid grid-cols-2 gap-3 w-full md:p-0 md:grid-cols-4 ">
             {data.categorias.map((c) => {
-                return <CategoryCard key={c.id} data={{
+              return (
+                <CategoryCard
+                  key={c.id}
+                  data={{
                     name: c.nombre,
                     description: c.description,
-                    image: c.image
-                }}/>
+                    image: c.image,
+                  }}
+                />
+              );
             })}
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
