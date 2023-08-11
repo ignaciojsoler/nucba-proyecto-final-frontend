786643f456087c1fb26ab89a76d54cacd7afee75
Ignacio Joaquin Soler <isoler@ia.com.mx>
2023-08-11 01:23:56 -0300

    Feature: service card grid

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -7,43 +7,29 @@
 //   userId: string;
 // }
 
-import carpineteria from "../assets/img/services/carpinteria.png";
-import cerrajeria from "../assets/img/services/cerrajeria.png";
-import electricidad from "../assets/img/services/electricidad.png";
-import gas from "../assets/img/services/gas.png";
-import jardineria from "../assets/img/services/jardineria.png";
-import limpieza from "../assets/img/services/limpieza.png";
-import mudanzas from "../assets/img/services/mudanzas.png";
-import plomeria from "../assets/img/services/plomeria.png";
-
-const categoriesImages = [
-  carpineteria,
-  cerrajeria,
-  electricidad,
-  gas,
-  jardineria,
-  limpieza,
-  mudanzas,
-  plomeria,
-];
-
-const ServiceCard = () => {
+interface ServiceCardProps {
+  idx: number;
+}
+const ServiceCard = ({ idx }: ServiceCardProps) => {
   return (
-    <div className="group rounded-lg overflow-hidden bg-slate-900 grid grid-cols-6 h-48">
-      <div className="space-y-1 p-6 col-span-4">
+    <div className="group rounded-lg overflow-hidden bg-slate-900 grid grid-cols-6 cursor-pointer transition duration-150 hover:bg-slate-800">
+      <div
+        className={`relative h-full col-span-6 flex justify-center items-center lg:col-span-2 lg:order-last bg-opacity-80 transition duration-150 group-hover:bg-opacity-60`}
+      >
+        <img
+          src=""
+          className="object-cover drop-shadow-xl h-40 z-10"
+          alt="user image"
+        />
+       <div className="absolute opacity-60 w-full h-full bg-gradient-to-b from-transparent to-slate-600"></div>
+      </div>
+      <div className="space-y-1 p-6 col-span-6 lg:col-span-4">
         <h4 className="font-bold text-3xl ">Título</h4>
-        <h5 className="font-bold text-xl text-emerald-600">$1000/h</h5>
+        <span className="text-xs">Carlos Pérez</span>
+        <h5 className={`font-bold text-xl text-emerald-600`}>$1000/h</h5>
         <p className=" text-slate-400">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, minima
-          quo. Dolorum veniam accusantium pariatur
         </p>
-      </div>
-      <div className=" bg-gradient-to-b from-emerald-600 to-emerald-800 h-48 col-span-2 col-start-5 flex justify-center items-center">
-        <img
-          src={categoriesImages[6]}
-          className="object-cover h-full"
-          alt="category image"
-        />
       </div>
     </div>
   );
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ServiceCard from './ServiceCard'
 
 const Services = () => {
+    const number = [0, 1,2,3,4,5,6,7]
   return (
     <article className="w-full space-y-4 py-4 animate-sladeInFromBottomLarge">
       <h4 className=" text-2xl font-semibold self-start">Servicios</h4>
-      <div className="grid grid-cols-1 gap-4 w-full md:p-0 md:grid-cols-2">
-        <ServiceCard/>
-        <ServiceCard/>
-        <ServiceCard/>
-        <ServiceCard/>
+      <div className="grid grid-cols-1 gap-6 w-full md:p-0 md:grid-cols-2">
+        {number.map((n, idx) => {
+            return <ServiceCard idx={idx}/>
+        })}
         {/* {data.categorias.map((c) => {
           return (
             <CategoryCard
