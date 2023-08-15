c38aa282f4fa7fc4e6215aa0a8100573744a85a0
Ignacio Joaquin Soler <ignaciojsoler@gmail.com>
2023-08-15 08:52:26 -0300

    Fix: landing text sizes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,11 +8,11 @@ const Landing = () => {
   return (
     <div className="m-auto w-full h-screen flex flex-col justify-center items-center">
       <section className="p-8 space-y-8 rounded-xl w-full z-10 pt-36 md:max-w-2xl lg:max-w-7xl md:pt-0">
-        <div className="space-y-6 md:max-w-2xl lg:max-w-xl"> 
-          <h1 className=" text-5xl font-bold lg:text-6xl animate-sladeInFromBottomShort">
+        <div className="space-y-6 md:max-w-2xl lg:max-w-2xl"> 
+          <h1 className=" text-5xl font-bold md:text-8xl animate-sladeInFromBottomShort">
             Bienvenido a <span className=" text-emerald-600">ServiHogar</span>
           </h1>
-          <p className=" leading-8 animate-sladeInFromBottomMedium">
+          <p className="text-xl md:text-2xl leading-8 animate-sladeInFromBottomMedium">
             ServiHogar es la plataforma perfecta para conectar a clientes como
             tú con los trabajadores más talentosos y confiables para satisfacer
             todas tus necesidades domésticas.
@@ -24,6 +24,7 @@ const Landing = () => {
           }}
           title="Empezar ahora"
           className="w-full lg:w-auto animate-sladeInFromBottomLong"
+          size="large"
         />
       </section>
       <div className="absolute m-auto top-0 max-h-screen overflow-hidden">
